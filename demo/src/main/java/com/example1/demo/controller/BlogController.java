e1d49746cf37ceb47b06d4439ce6bdac537931a7
alscjf2rnt <alscjf2rnt@naver.com>
2024-10-16 14:07:39 +0900

    6주차 게시판 수정(오류페이지 ppt수정필요)

--- demo/src/main/java/com/example1/demo/controller/BlogController.java
+++ demo/src/main/java/com/example1/demo/controller/BlogController.java
@@ -1,23 +1,51 @@
 package com.example1.demo.controller;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
 import java.util.List;
+import java.util.Optional;
+
 import com.example1.demo.model.domain.Article;
+import com.example1.demo.model.service.AddArticleRequest;
 import com.example1.demo.model.service.BlogService;
 
-@Controller   //컨트롤러 이노테이션 명시
+
+@Controller   // 컨트롤러 이노테이션 명시
 public class BlogController {
 
     @Autowired
     BlogService blogService; // DemoController 클래스 아래 객체 생성
 
-    @GetMapping("/article_list")   //게시판 링크 지정
+    @GetMapping("/article_list")   // 게시판 링크 지정
     public String article_list(Model model) {
-    List<Article> list = blogService.findAll(); // 게시판 리스트
-    model.addAttribute("articles", list); // 모델에 추가
-    return "article_list"; // .HTML 연결
+        List<Article> list = blogService.findAll(); // 게시판 리스트
+        model.addAttribute("articles", list); // 모델에 추가
+        return "article_list"; // .HTML 연결
     }
 
+    // 게시글 수정 화면 표시
+    @GetMapping("/article_edit/{id}") // 게시판 링크 지정
+    public String article_edit(Model model, @PathVariable Long id) {
+        Optional<Article> articleOptional = blogService.findById(id); // 선택한 게시판 글
+        if (articleOptional.isPresent()) {
+            model.addAttribute("article", articleOptional.get()); // 존재하면 Article 객체를 모델에 추가
+        } else {
+            // 처리할 로직 추가 (예: 오류 페이지로 리다이렉트, 예외 처리 등)
+            return "error"; // 오류 처리 페이지로 연결
+        }
+        return "article_edit"; // .HTML 연결
+    }
+
+    // 게시글 수정 처리
+    @PutMapping("/api/article_edit/{id}")                               
+    public String updateArticle(@PathVariable Long id, @ModelAttribute AddArticleRequest request) {
+        blogService.update(id, request);
+        return "redirect:/article_list"; // 글 수정 이후 .html 연결
+    }
 }
--- demo/src/main/java/com/example1/demo/model/service/BlogService.java
+++ demo/src/main/java/com/example1/demo/model/service/BlogService.java
@@ -1,6 +1,7 @@
 package com.example1.demo.model.service;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example1.demo.model.domain.Article;
@@ -13,11 +14,11 @@ public class BlogService {
     @Autowired // 객체 주입 자동화, 생성자 1개면 생략 가능
     private final BlogRepository blogRepository; // 리포지토리 선언
 
-        public List<Article> findAll() { // 게시판 전체 목록 조회
+    public List<Article> findAll() { // 게시판 전체 목록 조회
         return blogRepository.findAll();   
     }
 
-    public Article save(AddArticleRequest request){
+    public Article save(AddArticleRequest request) {
         // DTO가 없는 경우 이곳에 직접 구현 가능
         // public ResponseEntity<Article> addArticle(@RequestParam String title, @RequestParam String content) {
         // Article article = Article.builder()
@@ -25,6 +26,19 @@ public class BlogService {
         // .content(content)
         // .build();
         return blogRepository.save(request.toEntity());
-        }
-        
+    }
+
+    // 게시판 특정 글 조회
+    public Optional<Article> findById(Long id) { 
+        return blogRepository.findById(id);
+    }
+
+    // 게시글 수정
+    public void update(Long id, AddArticleRequest request) {
+        Optional<Article> optionalArticle = blogRepository.findById(id); // 단일 글 조회
+        optionalArticle.ifPresent(article -> { // 값이 있으면
+            article.update(request.getTitle(), request.getContent()); // 값을 수정
+            blogRepository.save(article); // Article 객체에 저장
+        });
+    }
 }
